4a18a40966523828e1821436f9550c6f3054b7c9
Julio Mendez <juliolilo2002rgmail.com>
2022-07-28 23:15:37 +0200

    v1

---
+++ IyC.js
@@ -0,0 +1,55 @@
+const nombre = document.getElementById("nombre");
+const empresa= document.getElementById("empresa");
+const email= document.getElementById("email");
+const telefono= document.getElementById("telefono");
+const mensaje= document.getElementById("mensaje");
+const submit= document.getElementById("submit");
+
+
+const resultado = document.querySelector(".resultado");
+
+
+submit.addEventListener("click", (e)=>{
+    /*alert("hola");*/
+    e.preventDefault();
+
+    let error = validarCampos();  
+    if(error[0]){
+
+       resultado.innerHTML = error[1];
+       resultado.classList.add("red");
+       resultado.classList.remove("red");
+
+        
+    }else{
+
+        resultado.innerHTML = "Solicitud enviada correctamente";
+        resultado.classList.add("green");
+    }
+})
+
+
+const validarCampos= ()=>{
+
+    let error= [];
+    if(nombre.value.length < 5 || nombre.value.length > 40 ){
+        error[0]= true;
+        error[1]= "el nombre no puede tener menos de 5 caracteres o mas de 40";
+        
+        return error;
+
+
+    }
+    else if(email.value.length <5 || email.value.length > 40){
+        error[0]= true;
+        error[1]= "el email no puede tener menos de 5 caracteres o mas de 40";
+
+        return error;
+    }
+    else{
+        error[0]=false;
+        return error;
+
+    }
+    
+}
---
+++ mama.js
@@ -0,0 +1,18 @@
+/*let valor= prompt("hola");
+alert(valor);*/
+
+a= "paco ";
+b= "me la chupa";
+x=3;
+c=" veces";
+
+/*document.write(a + b+ x +c);*/
+
+miArray = ["pepe","juan"];
+/*document.write(miArray[1]);*/
+
+let paco= screen.availHeight;
+console.log(paco);
+
+let ruta= window.location.pathname;
+console.log(ruta);
