26a7ec245495ba037e99c58723f1d0a2a89502f9
Julio Mendez <juliolilo2002rgmail.com>
2022-08-28 13:03:02 +0200

    Cambios de diseño

---
+++ quienes-somos.js
@@ -0,0 +1,12 @@
+ 
+
+
+ventaja1.addEventListener("click", () => {
+        alert("hola");
+        ventaja1.style.top = ventaja1.style.top +10;
+});
+
+
+window.addEventListener("scroll", function(){
+    var ventaja1 =document.querySelector(".ventaja1");
+});
---
+++ visitas_guiadas.js
@@ -0,0 +1,6 @@
+const prado = document.querySelector(".museoDelPrado");
+const url = "";
+prado.addEventListener("click", ()=>{
+    //alert("holaaa");
+    window.open("visitas-guiadas/Prado.html");
+})
